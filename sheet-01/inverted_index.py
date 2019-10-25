2d93b0490e71a0421b9d4ad55d44c48cab26cc87
Najji <najji_hindo@hotmail.com>
2019-10-25 13:08:42 +0200

    Exercise 1

---
+++ sheet-01/inverted_index.py
@@ -0,0 +1,162 @@
+"""
+Copyright 2019, University of Freiburg
+Hannah Bast <bast@cs.uni-freiburg.de>
+Claudius Korzen <korzen@cs.uni-freiburg.de>
+Patrick Brosi <brosi@cs.uni-freiburg.de>
+"""
+
+import re
+import readline  # NOQA
+import sys
+
+
+class InvertedIndex:
+    """
+    A simple inverted index as explained in lecture 1.
+    """
+
+    def __init__(self):
+        """
+        Creates an empty inverted index.
+        """
+        self.inverted_lists = {}  # The inverted lists of record ids.
+
+    def read_from_file(self, file_name):
+        """
+        Constructs the inverted index from given file in linear time (linear in
+        the number of words in the file). The expected format of the file is
+        one record per line, in the format <title>TAB<description>.
+
+        TODO: Make sure that each inverted list contains a particular record id
+        at most once, even if the respective word occurs multiple times in the
+        same record.
+
+        >>> ii = InvertedIndex()
+        >>> ii.read_from_file("example.txt")
+        >>> sorted(ii.inverted_lists.items())
+        [('a', [1, 2]), ('doc', [1, 2, 3]), ('film', [2]), ('movie', [1, 3])]
+        """
+        with open(file_name, "r") as file:
+            record_id = 0
+            for line in file:
+                line = line.strip()
+                record_id += 1
+
+                for word in re.split("[^A-Za-z]+", line):
+                    word = word.lower().strip()
+
+                    # Ignore the word if it is empty.
+                    if len(word) == 0:
+                        continue
+
+                    if word not in self.inverted_lists:
+                        # The word is seen for first time, create a new list.
+                        self.inverted_lists[word] = []
+
+                    # list contains a particular record id at most once.
+                    elif record_id in self.inverted_lists[word]:
+                        continue
+                    self.inverted_lists[word].append(record_id)
+
+    def intersect(self, list1, list2):
+        """
+        Computes the intersection of the two given inverted lists in linear
+        time (linear in the total number of elements in the two lists).
+
+        >>> ii = InvertedIndex()
+        >>> ii.intersect([1, 5, 7], [2, 4])
+        []
+        >>> ii.intersect([1, 2, 5, 7], [1, 3, 5, 6, 7, 9])
+        [1, 5, 7]
+        """
+        pass  # TODO: add your code here
+        result = []
+        i = j = 0
+        while (i < len(list1) and j < len(list2)):
+            if list1[i] == list2[j]:
+                result.append(list1[i])
+                i += 1
+                j += 1
+            elif list1[i] < list2[j]:
+                i += 1
+            else:
+                j += 1
+
+        return result
+
+    def process_query(self, keywords):
+        """
+        Processes the given keyword query as follows: Fetches the inverted list
+        for each of the keywords in the given query and computes the
+        intersection of all inverted lists (which is empty, if there is a
+        keyword in the query which has no inverted list in the index).
+
+        >>> ii = InvertedIndex()
+        >>> ii.read_from_file("example.txt")
+        >>> ii.process_query([])
+        []
+        >>> ii.process_query(["doc", "movie"])
+        [1, 3]
+        >>> ii.process_query(["doc", "movie", "comedy"])
+        []
+        """
+        pass  # TODO: add your code here
+        result = []
+        if len(keywords) < 2:
+            return result
+
+        list1 = self.inverted_lists[keywords[0]]
+        list2 = self.inverted_lists[keywords[1]]
+        result = self.intersect(list1, list2)
+        for i in range(2, len(keywords)):
+            if keywords[i] in self.inverted_lists.keys():
+                tmp = self.inverted_lists[keywords[i]]
+                result = self.intersect(result, tmp)
+                continue
+            else:
+                # if  a keyword in the query has no inverted list in the index.
+                return []
+        return result
+
+    def main(self):
+        """
+        >>> ii = InvertedIndex()
+        >>> ii.main()
+
+
+        """
+        file_name = input("put file name : ")
+        self.read_from_file(file_name)
+        print(self.inverted_lists)
+
+        with open(file_name, "r") as file:
+            word = input()
+            if word in self.inverted_lists:
+                number = self.inverted_lists[word]
+            if len(number) == 0:
+                print("No Match")
+            # TODO: print the frist three matches
+                return
+            record_id = 0
+            for line in file:
+                line = line.strip()
+                if (record_id == number[0]-1):
+                    print(line)
+                record_id += 1
+
+
+
+
+if __name__ == "__main__":
+    # Parse the command line arguments.
+    if len(sys.argv) != 2:
+        print("Usage: python3 inverted_index.py <file>")
+        sys.exit()
+
+    file_name = sys.argv[1]
+    ii = InvertedIndex()
+    ii.main()
+    ii.read_from_file(file_name)
+
+    for word, inverted_list in ii.inverted_lists.items():
+        print("%d\t%s" % (len(inverted_list), word))

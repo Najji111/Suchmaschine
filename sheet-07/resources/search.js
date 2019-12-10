c32a5c0361dbe77252eec6a6a39a62f09d5bbf7d
Jesus von Maria <jesus@linux-2.fritz.box>
2019-12-10 04:19:27 +0100

    es6

--- sheet-07/resources/search.js
+++ sheet-07/resources/search.js
@@ -3,20 +3,49 @@ $(document).ready(function(){
 
 $("#field").keyup(function(){
   /* one types a query */
-  console.log("Input: " + $("#field").val())
+  console.log("Input: " + $("#field").val());
   // create url from input
   var host = window.location.hostname;
   var port = window.location.port;
   var query = $("#field").val();
-  var url = "http://" + host + ":" + port + "/search.html/api?q=" + query;
+  var url = "http://" + host + ":" + port + "/api?q=" + query;
 
   /* send get-request with query.
-  After receiving the response call the function, */
-  $.get(url, function(response){
-    console.log(response);
-    //$("#result").html(response.result);
-  })
-})
-})
+  After receiving the response call the function. */
+ $.get(url, function(res){
+    console.log("resp: " + res.QUERY);
+//    var myObj = JSON.parse(response);
+//    console.log(myObj);
+    $("#result-header").html(res.QUERY);
+
+  // style all results
+  var entity = ""
+  for (var i = 0; i < res.ENTITYS.length; i++){ 
+    console.log(i)
+    var res_e = res.ENTITYS[i];
+
+    entity += '<div class=\"entity\"> ' +
+      '  <div class="entity-image"><a target="_blank" href="' +
+      res_e.ENTITY_WIKIDATA_URL + '"><img src="' +  res_e.ENTITY_IMG + '"></div></a>' +
+      '  <div class="entity-metadata">' +
+      '    <div class="entity-header">' +
+      '      <div class="entity-name">' + res_e.ENTITY_NAME + '</div>' +
+      '      <div class="entity-synonym">' + res_e.ENTITY_SYNONYM + ' + (ped ' + res_e.ENTITY_PED + ', score ' + res_e.ENTITY_SCORE + ')</div>' +
+      '    </div>' +
+      '    <div class="entity-description">' + res_e.ENTITY_DESC + '</div>' +
+      '    <div class="entity-links">' +
+      '      External Links:' +
+      '      <a target="_blank" href="' + res_e.ENTITY_WIKIDATA_URL + '"><img src="wikidata.png"></a>' +
+      '      <a target="_blank" href="' + res_e.ENTITY_WIKIPEDIA_URL + '"><img src="wikipedia.png"></a>' +
+      '    </div>' +
+      '  </div>' +
+      '</div>`';
+  }
+  // show results
+  console.log(entity)
+  $("#result-body").html(entity);
+  });
+});
+});
 
 
--- sheet-07/search_server.py
+++ sheet-07/search_server.py
@@ -12,221 +12,216 @@ import time
 import mimetypes
 import qgram_index
 from pathlib import Path
+from urllib.parse import unquote
 
 
 class SearchServer:
-  """
-  A HTTP search server using a q gram index.
-  """
-  def __init__(self, port, qi):
-    '''
-    Inits a simple HTTP search server
-    '''
+    """
+    A HTTP search server using a q gram index.
+    """
+    def __init__(self, port, qi):
+        '''
+        Inits a simple HTTP search server
+        '''
 
-    self.port = port
-    self.qi = qi
+        self.port = port
+        self.qi = qi
 
-  def run(self):
-    try:
-      sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-      server_address = ('0.0.0.0', self.port)
-      sock.bind(server_address)
-      sock.listen(1)
-
-      while True:
-        print("Waiting on port %i" % port)
-        conn, client_address = sock.accept()
-        # connection timeout 5 seconds
-        conn.settimeout(5.0)
+    def run(self):
         try:
-          print("Client connected from %s" % client_address[0])
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-          data = bytearray()
-          while True:
-            batch = conn.recv(32)
+            server_address = ('0.0.0.0', self.port)
+            sock.bind(server_address)
+            sock.listen(1)
 
-            if batch:
-              data.extend(batch)
-              if data.find(b'\r\n\r\n') > -1:
-                break
-            else:
-              break
+            while True:
+                print("Waiting on port %i" % port)
+                conn, client_address = sock.accept()
+                # connection timeout 5 seconds
+                conn.settimeout(5.0)
+                try:
+                    print("Client connected from %s" % client_address[0])
 
-          self.handle_request(conn, data[0:data.find(b'\r\n')])
-        except socket.timeout:
-          print("(Timeout from %s)" % client_address[0])
-          pass
-        except:
-          raise
+                    data = bytearray()
+                    while True:
+                        batch = conn.recv(32)
+
+                        if batch:
+                            data.extend(batch)
+                            if data.find(b'\r\n\r\n') > -1:
+                                break
+                        else:
+                            break
+
+                    self.handle_request(conn, data[0:data.find(b'\r\n')])
+                except socket.timeout:
+                    print("(Timeout from %s)" % client_address[0])
+                    pass
+                finally:
+                    conn.close()
         finally:
-          conn.close()
-    finally:
-      print("Closing socket...")
-        sock.close()
-  
-  def handle_search(self, query):
-      """# handle searches
-      content = content.decode()
-      with open("resources/entity.pattern.html", "r") as patf:
-        pattern = patf.read()
+            print("Closing socket...")
+            sock.close()
 
-      query = self.qi.normalize(params.get("q", ""))
+    def url_decode(self, str):
+        '''
+        Decode an URL-encoded UTF-8 string, as explained in the lecture.
+        Don't forget to also decode a "+" (plus sign) to a space (" ")!
 
-      start = time.monotonic()
+        >>> s = SearchServer(0, None)
+        >>> s.url_decode("nirwana")
+        'nirwana'
+        >>> s.url_decode("the+m%C3%A4trix")
+        'the mätrix'
+        >>> s.url_decode("Mikr%C3%B6soft+Windos")
+        'Mikrösoft Windos'
+        >>> s.url_decode("The+hitschheiker%20guide")
+        'The hitschheiker guide'
+        '''
+        res = unquote(str).replace("+", " ")
+        return res
 
-      # Process the keywords.
-      delta = int(len(query) / 4)
+    def handle_search(self, filep, query):
+        """ Searches for the query and returns a the result as a json.
 
-      postings = self.qi.find_matches(query, delta)
+        >>> q = qgram_index.QGramIndex(3, False)
+        >>> q.build_from_file("wiki-test.tsv")
+        >>> filep = Path(__file__).parent.absolute() / Path("./api")
+        >>> ss = SearchServer(8081, q)
+        >>> ss.handle_search(filep, "germany")
+        ... # doctest: +NORMALIZE_WHITESPACE
+        bytearray(b'{ "QUERY": "germany", "ENTITYS": [ { "ENTITY_NAME":
+        "Germany", "ENTITY_SYNONYM": "Germany", "ENTITY_SCORE": "347",
+        "ENTITY_PED": "0", "ENTITY_DESC": "country in central Europe",
+        "ENTITY_IMG": "http//...", "ENTITY_WIKIDATA_URL":
+        "https://www.wikidata.org/wiki/Q183", "ENTITY_WIKIPEDIA_URL":
+        "https://..." }, { "ENTITY_NAME": "germania", "ENTITY_SYNONYM":
+        "germania", "ENTITY_SCORE": "346", "ENTITY_PED": "1", "ENTITY_DESC":
+        "country", "ENTITY_IMG": "http://...", "ENTITY_WIKIDATA_URL":
+        "https://www.wikidata.org/wiki/Q142", "ENTITY_WIKIPEDIA_URL":
+        "https://..." } ] }')
+"""
 
-      html = ""
-      for p in q.rank_matches(postings)[:5]:
-        html += pattern
-        html = html.replace("%ENTITY_NAME%", (q.entities[p[0] - 1][0]))
-        html = html.replace("%ENTITY_SYNONYM%", (q.names[p[3] - 1]))
-        html = html.replace("%ENTITY_SCORE%", (str(p[2])))
-        html = html.replace("%ENTITY_PED%", (str(p[1])))
-        html = html.replace("%ENTITY_DESC%", (q.entities[p[0] - 1][2]))
-        html = html.replace("%ENTITY_IMG%", (q.entities[p[0] - 1][6]))
-        html = html.replace("%ENTITY_WIKIDATA_URL%",
-                  ("https://www.wikidata.org/wiki/%s"
-                   % q.entities[p[0] - 1][4]))
-        html = html.replace("%ENTITY_WIKIPEDIA_URL%",
-                  (q.entities[p[0] - 1][3]))
+        if filep.name != "api":
+            # read request file as bytearray
+            with filep.open("rb") as reqf:
+                content = reqf.read()
+        else:
+            # handle searches
 
-      content = content.replace(
-        "%RESULT_HEADER%",
-        "Got %i result(s), merged %i lists with %i elements"
-        "(%i ms), %i ped calculations (%i ms), took %i ms total."
-        % (len(postings), q.merges[0], q.merges[1], q.merge_time,
-          q.ped_calcs[0], q.ped_time,
-          (time.monotonic() - start) * 1000))
-      content = content.replace("%QUERY%", query)
-      content = content.replace("%RESULT%", html)
+            # Process the keywords.
+            delta = int(len(query) / 4)
 
-      # back to bytearray
-      content = bytearray(content, "utf-8")
-    """
-    
-    
-  def handle_request(self, con, req):
-     print("Handling request: %s" % req.decode("utf-8"))
-    
-    # we only support GET
-    meth = req.split(b" ")[0].decode("utf-8")
-    if meth != "GET":
-        con.sendall(b'HTTP/1.1 405 Not Allowed\r\n'
-            b'Content-length: 12\r\n\r\nNot Allowed!')
-      return
+            postings = self.qi.find_matches(query, delta)
+            json = ""
+            for p in self.qi.rank_matches(postings)[:5]:
+                if json != "":
+                    json += ","
+                else:
+                    json += "{ \"QUERY\": \"" + query
+                    json += "\", \"ENTITYS\": ["
 
-    path = req.split(b" ")[1].decode("utf-8")
-    s = path.split("/api?")
-    path = s[0]
+                json += " { \"ENTITY_NAME\": \"" + \
+                    self.qi.entities[p[0] - 1][0]
+                json += "\", \"ENTITY_SYNONYM\": \"" + self.qi.names[p[3] - 1]
+                json += "\", \"ENTITY_SCORE\": \"" + str(p[2])
+                json += "\", \"ENTITY_PED\": \"" + str(p[1])
+                json += "\", \"ENTITY_DESC\": \"" + \
+                    self.qi.entities[p[0] - 1][2]
+                json += "\", \"ENTITY_IMG\": \"" + \
+                    self.qi.entities[p[0] - 1][6][:-1]
+                json += "\", \"ENTITY_WIKIDATA_URL\": \"" + \
+                    ("https://www.wikidata.org/wiki/%s" %
+                        self.qi.entities[p[0] - 1][4])
+                json += "\", \"ENTITY_WIKIPEDIA_URL\": \"" + \
+                    self.qi.entities[p[0] - 1][3]
+                json += "\" }"
+            json += " ] }"
 
-    params = {}
-    if len(s) > 1:
-      params = dict([tuple(a.split("=")) for a in s[1].split("&")])
+            # prevent xss
+            # regex = re.compile("<script>.*<script/>")
+            # result = re.findall(regex, json)
 
-    # ensure path is relative to CWD/resources/
-    filep = Path(__file__).parent.absolute() / Path("./resources/" + path)
+            # back to bytearray
+            content = bytearray(json, "utf-8")
+        return content
 
-    print(path)
-    # return 404 if file does not exist
-    if not filep.exists():
-      con.sendall(b'HTTP/1.1 404 Not found\r\n'
-            b'Content-length: 10\r\n\r\nNot found!')
-      return
+    def handle_request(self, con, req):
+        print("Handling request: %s" % req.decode("utf-8"))
 
-    # return 403 for dir requests
-    if not filep.is_file():
-      con.sendall(b'HTTP/1.1 403 Forbidden\r\n'
-            b'Content-length: 12\r\n\r\nNot allowed!')
-      return
+        # we only support GET
+        meth = req.split(b" ")[0].decode("utf-8")
+        if meth != "GET":
+            con.sendall(b'HTTP/1.1 405 Not Allowed\r\n'
+                        b'Content-length: 12\r\n\r\nNot Allowed!')
+            return
 
-    # guess MIME type
-    t = bytearray(mimetypes.guess_type(str(filep.resolve()))[0], "utf-8")
+        path = req.split(b" ")[1].decode("utf-8")
+        s = path.split("?")
+        path = s[0]
 
-    # return headers
-    data = bytearray('HTTP/1.1 200 OK\r\nContent-type: %s\r\n' % t,
-             "utf-8")
+        params = {}
+        if len(s) > 1:
+            params = dict([tuple(a.split("=")) for a in s[1].split("&")])
 
-    # read request file as bytearray
-    with filep.open("rb") as reqf:
-      content = reqf.read()
+        # ensure path is relative to CWD/resources/
+        filep = Path(__file__).parent.absolute() / Path("./resources/" + path)
 
-    if filep.name == "search.html":
-         
-      """# handle searches
-      content = content.decode()
-      with open("resources/entity.pattern.html", "r") as patf:
-        pattern = patf.read()
+        if filep.name != "api":
+            # return 404 if file does not exist
+            if not filep.exists():
+                con.sendall(b'HTTP/1.1 404 Not found\r\n'
+                            b'Content-length: 10\r\n\r\nNot found!')
+                return
 
-      query = self.qi.normalize(params.get("q", ""))
+            # return 403 for dir requests
+            if not filep.is_file():
+                con.sendall(b'HTTP/1.1 403 Forbidden\r\n'
+                            b'Content-length: 12\r\n\r\nNot allowed!')
+                return
 
-      start = time.monotonic()
+            # guess MIME type
+            t = bytearray(mimetypes.guess_type(str(filep.resolve()))[0],
+                          "utf-8")
+            query = ""
+        else:
+            query = self.url_decode(params.get("q", ""))
+            query = self.qi.normalize(query)
+            # it is a query request
+            t = "application/json"
 
-      # Process the keywords.
-      delta = int(len(query) / 4)
+        # return headers
+        data = bytearray('HTTP/1.1 200 OK\r\nContent-type: %s\r\n' % t,
+                         "utf-8")
 
-      postings = self.qi.find_matches(query, delta)
+        content = self.handle_search(filep, query)
 
-      html = ""
-      for p in q.rank_matches(postings)[:5]:
-        html += pattern
-        html = html.replace("%ENTITY_NAME%", (q.entities[p[0] - 1][0]))
-        html = html.replace("%ENTITY_SYNONYM%", (q.names[p[3] - 1]))
-        html = html.replace("%ENTITY_SCORE%", (str(p[2])))
-        html = html.replace("%ENTITY_PED%", (str(p[1])))
-        html = html.replace("%ENTITY_DESC%", (q.entities[p[0] - 1][2]))
-        html = html.replace("%ENTITY_IMG%", (q.entities[p[0] - 1][6]))
-        html = html.replace("%ENTITY_WIKIDATA_URL%",
-                  ("https://www.wikidata.org/wiki/%s"
-                   % q.entities[p[0] - 1][4]))
-        html = html.replace("%ENTITY_WIKIPEDIA_URL%",
-                  (q.entities[p[0] - 1][3]))
-
-      content = content.replace(
-        "%RESULT_HEADER%",
-        "Got %i result(s), merged %i lists with %i elements"
-        "(%i ms), %i ped calculations (%i ms), took %i ms total."
-        % (len(postings), q.merges[0], q.merges[1], q.merge_time,
-          q.ped_calcs[0], q.ped_time,
-          (time.monotonic() - start) * 1000))
-      content = content.replace("%QUERY%", query)
-      content = content.replace("%RESULT%", html)
-
-      # back to bytearray
-      content = bytearray(content, "utf-8")
-    """
-      content =   
-
-    # we finally now the content length
-    data.extend(b'Content-length: %i\r\n\r\n' % len(content))
-    data.extend(content)
-
-    # send answer
-    con.sendall(data)
+        # we finally now the content length
+        data.extend(b'Content-length: %i\r\n\r\n' % len(content))
+        data.extend(content)
+        # send answer
+        con.sendall(data)
 
 
 if __name__ == "__main__":
-  # Parse the command line arguments.
-  if len(sys.argv) < 3:
-    print("Usage: python3 %s <file> <port> [--use-synonyms]" % sys.argv[0])
-    sys.exit()
+    # Parse the command line arguments.
+    if len(sys.argv) < 3:
+        print("Usage: python3 %s <file> <port> [--use-synonyms]" % sys.argv[0])
+        sys.exit()
 
-  file_name = sys.argv[1]
-  port = int(sys.argv[2])
+    file_name = sys.argv[1]
+    port = int(sys.argv[2])
 
-  use_synonyms = "--use-synonyms" in sys.argv
+    use_synonyms = "--use-synonyms" in sys.argv
 
-  print("Reading from file '%s'." % file_name)
-  start = time.monotonic()
-  q = qgram_index.QGramIndex(3, use_synonyms)
-  q.build_from_file(file_name)
-  print("Done, took %i ms." % ((time.monotonic() - start) * 1000))
+    print("Reading from file '%s'." % file_name)
+    start = time.monotonic()
+    q = qgram_index.QGramIndex(3, use_synonyms)
+    q.build_from_file(file_name)
+    print("Done, took %i ms." % ((time.monotonic() - start) * 1000))
 
-  s = SearchServer(port, q)
-  print("Starting server, go to http://localhost:%i/search.html ..." % port)
-  s.run()
+    s = SearchServer(port, q)
+    print("Starting server, go to http://localhost:%i/search.html ..." % port)
+    s.run()
